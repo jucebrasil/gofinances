770b65b0b3893b55d0cf78ba5f47104cd3fb6946
Juce Brasil <jucebrasil.ti@gmail.com>
2021-08-19 21:19:59 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'react-native';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
+
+import { SignIn } from './src/screens/SignIn';
+
+import theme from './src/global/styles/theme';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" />
+        <SignIn />
+        {/* <AppRoutes /> */}
+      </NavigationContainer>
+    </ThemeProvider>
+
+  )
+}
+
